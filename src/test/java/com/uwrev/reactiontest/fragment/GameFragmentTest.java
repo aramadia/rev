b46228599d55f30f284f88f8b62c96f50ee1f63c
Josh Lauer <jlauer79@gmail.com>
2014-04-06 15:17:56 -0700

    Removing old code, new version of robolectric has this

--- src/test/java/com/uwrev/reactiontest/fragment/GameFragmentTest.java
+++ src/test/java/com/uwrev/reactiontest/fragment/GameFragmentTest.java
@@ -1,12 +1,7 @@
 package com.uwrev.reactiontest.fragment;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
 import static junit.framework.Assert.assertNotNull;
@@ -21,22 +16,9 @@ public class GameFragmentTest {
   @Test
   public void shouldNotBeNull() throws Exception {
     GameFragment myFragment = new GameFragment();
-    startMyFragment(myFragment);
+    startFragment(myFragment);
 
     assertNotNull(myFragment);
-  }
-
-  public void startMyFragment(Fragment fragment) {
-    FragmentActivity activity = Robolectric.buildActivity(FragmentActivity.class)
-        .create()
-        .start()
-        .resume()
-        .get();
-
-    FragmentManager fragmentManager = activity.getSupportFragmentManager();
-    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-    fragmentTransaction.add(fragment, null);
-    fragmentTransaction.commit();
   }
 
 }
