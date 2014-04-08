0ce2290a66408d0d22f793f8403e53a26a15b85a
Daniel Lam <lamdanielalex@gmail.com>
2014-04-07 23:27:27 -0700

    facebook login

--- src/main/java/com/uwrev/reactiontest/fragment/ScoreFragment.java
+++ src/main/java/com/uwrev/reactiontest/fragment/ScoreFragment.java
@@ -1,27 +1,34 @@
 package com.uwrev.reactiontest.fragment;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
+import com.parse.*;
 import com.uwrev.reactiontest.R;
+import com.uwrev.reactiontest.ReactionApplication;
 import com.uwrev.reactiontest.UserManager;
+import com.uwrev.reactiontest.activity.MainActivity;
+import com.uwrev.reactiontest.activity.UserDetailsActivity;
 import com.uwrev.reactiontest.adapter.ScoreAdapter;
 import com.uwrev.reactiontest.model.RevScore;
 
 import javax.inject.Inject;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -35,6 +42,8 @@ public class ScoreFragment extends ReactionBaseFragment {
   @Inject UserManager userManager;
 
   private ScoreAdapter adapter;
+
+  private Dialog progressDialog;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -94,6 +103,21 @@ public class ScoreFragment extends ReactionBaseFragment {
 
   }
 
+  @OnClick(R.id.button_facebook)
+  public void connectFacebook() {
+    // check if already connected
+
+    // Check if there is a currently logged in user
+    // and they are linked to a Facebook account.
+    ParseUser currentUser = ParseUser.getCurrentUser();
+    if ((currentUser != null) && ParseFacebookUtils.isLinked(currentUser)) {
+      // Go to the user info activity
+    }
+    else {
+      onLoginButtonClicked();
+    }
+  }
+
   private void getScoresAsync() {
     scoreList.setVisibility(View.GONE);
     progressSpinner.setVisibility(View.VISIBLE);
@@ -109,6 +133,36 @@ public class ScoreFragment extends ReactionBaseFragment {
           adapter.setScores(revScores);
         } else {
           Log.e("score", "Error: " + e.getMessage());
+        }
+      }
+    });
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+  }
+
+  private void onLoginButtonClicked() {
+    ScoreFragment.this.progressDialog = ProgressDialog.show(
+       getActivity(), "", "Logging in...", true);
+    List<String> permissions = Arrays.asList("basic_info", "user_about_me",
+        "user_relationships", "user_birthday", "user_location");
+    ParseFacebookUtils.logIn(permissions, getActivity(), new LogInCallback() {
+      @Override
+      public void done(ParseUser user, ParseException err) {
+        ScoreFragment.this.progressDialog.dismiss();
+        if (user == null) {
+          Log.d(ReactionApplication.TAG,
+              "Uh oh. The user cancelled the Facebook login.");
+        } else if (user.isNew()) {
+          Log.d(ReactionApplication.TAG,
+              "User signed up and logged in through Facebook!");
+
+        } else {
+          Log.d(ReactionApplication.TAG,
+              "User logged in through Facebook!");
         }
       }
     });
